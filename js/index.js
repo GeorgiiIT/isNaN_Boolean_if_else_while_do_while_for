06848621e5156df91fa6ceb4f73774c1b8173250
GeorgiiIT <boshkovgeorgii@gmail.com>
2022-03-25 08:35:25 +0200

    ready

--- js/index.js
+++ js/index.js
@@ -1,56 +1,44 @@
 userConf = confirm(`Tell me three most important words 💚`)
 
 if (userConf) {
-	for (word = 1; word <= 3; word++) {
-		do {
-			mainWord = prompt(`Enter word #${word}`);
-			letters = true;
-			if (mainWord) {
-				for (i = 0; i < 9; i++) {
-					if (mainWord.includes(i)) {
-						letters = false;
-						break;
-					}
-				}
-			}
-		} while (!Boolean(mainWord) || mainWord === null || !letters)
-		console.log(`Word #${word}: ${mainWord}`)
-		do {
-			typeTransform = prompt(`Choose type of transfromation for "${mainWord}": uppercase|lowercase|capitalize`, `uppercase`)
+    finalString = ``;
 
-			if (typeTransform === `uppercase`) {
-				typeTransformWord = mainWord.toUpperCase();
+    for (word = 1; word <= 3; word++) {
+        do {
+            mainWord = prompt(`Enter word #${word}`);
+            letters = true;
+            if (mainWord) {
+                for (i = 0; i < 9; i++) {
+                    if (mainWord.includes(i)) {
+                        letters = false;
+                        break;
+                    }
+                }
+            }
+        } while (!mainWord || !letters)
+        console.log(`Word #${word}: ${mainWord}`);
 
-			} else if (typeTransform === `lowercase`) {
-				typeTransformWord = mainWord.toLowerCase();
+        do {
+            typeTransform = prompt(`Choose type of transfromation for "${mainWord}": uppercase|lowercase|capitalize`, `uppercase`)
 
-			} else if (typeTransform === `capitalize`) {
-				typeTransformWord = mainWord.charAt(0).toUpperCase() + mainWord.slice(1);
+            if(typeTransform)
+                typeTransform = typeTransform.toLowerCase().replaceAll(` `,``);
 
-			} else typeTransform === ``
+            if (typeTransform === `uppercase`) {
+                typeTransformWord = mainWord.toUpperCase();
+            } else if (typeTransform === `lowercase`) {
+                typeTransformWord = mainWord.toLowerCase();
+            } else if (typeTransform === `capitalize`) {
+                typeTransformWord = mainWord.charAt(0).toUpperCase() + mainWord.slice(1);
+            }
+        } while (!typeTransform || typeTransform !== `uppercase` && typeTransform !== `lowercase` && typeTransform !== `capitalize`)
 
-		} while (!typeTransform || typeTransform !== `uppercase` && typeTransform !== `lowercase` && typeTransform !== `capitalize`)
+        console.log(`Transform type for word #${word}: ${typeTransform}`);
+        console.log(`Transformed word #${word}: ${typeTransformWord}`);
 
-		if (word === 1) {
-			firstWord = typeTransformWord;
-			
-			console.log(`Transform type for word #${word}: ${typeTransform}`)
-			console.log(`Transformed word #${word}: ${typeTransformWord}`)
-			console.log(`Sentanse: ${firstWord}`)
-		} else if (word === 2) {
-			secondWord = typeTransformWord;
-			
-			console.log(`Transform type for word #${word}: ${typeTransform}`)
-			console.log(`Transformed word #${word}: ${typeTransformWord}`)
-			console.log(`Sentanse: ${firstWord} ${secondWord}`)
-		} else if (word === 3) {
-			thirdWord = typeTransformWord;
-			
-			console.log(`Transform type for word #${word}: ${typeTransform}`)
-			console.log(`Transformed word #${word}: ${typeTransformWord}`)
-			console.log(`Sentanse: ${firstWord} ${secondWord} ${thirdWord}!`)
-		}
-	}
-	console.log(`******`);
-	console.log(`${firstWord} ${secondWord} ${thirdWord}!`);
+        finalString += typeTransformWord;
+        finalString += word === 3 ? `!` : ` `;
+    }
+    console.log(`******`);
+    console.log(finalString);
 }
